87a8f7f2704d1a4e9a29f46c4a632c91d73aece1
Anderson <anderson.nobrega62@gmail.com>
2019-05-13 21:56:01 -0300

    Adicionando arquivos csv, script main e script para leitura dos csv

---
+++ Grafo.py
@@ -0,0 +1,37 @@
+import csv
+
+def retornaGrafo():
+    grafo = {}
+
+    with open("csv/arestas.csv", "r") as csv_file:
+        tabela = csv.reader(csv_file)
+        for linha in tabela:
+            if linha[0] in grafo:
+                grafo[linha[0]].append(linha[1])
+            else:
+                grafo[linha[0]] = [linha[1]]
+
+    return grafo
+
+
+def heuristicaDistancia():
+    heuristica_csv = open("csv/heuristicaDistancia.csv")
+    matDistancia = []
+
+    for j in heuristica_csv:
+        sep = j.split(',')
+        sep[len(sep) - 1] = sep[len(sep) - 1].replace('\n', '')
+        sep.remove(sep[0])
+        matDistancia.append(sep)
+
+    ordenacaoCidades = matDistancia[0]
+    matDistancia.remove(matDistancia[0])
+
+    for i in range(len(matDistancia)):
+        for j in range(len(matDistancia[i])):
+            if j >= i:
+                matDistancia[i][j] = float(matDistancia[i][j])
+            else:
+                matDistancia[i][j] = None
+
+    return ordenacaoCidades, matDistancia
